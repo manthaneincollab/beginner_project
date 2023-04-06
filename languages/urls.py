0da38827193541de5df6ecb30c5c1c1e521a0183
Michelle Labasbas <manthanein@gmail.com>
2023-04-06 10:47:29 +0000

    error in URL pattern

--- languages/urls.py
+++ languages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView
 
 urlpatterns = [
-    path("", homePageView, name="home"),
+    path("", HomePageView, name="home"),
 ]
--- languages/views.py
+++ languages/views.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-
-def homePageView(request):
-    return HttpResponse("Hello, World!")
-
-
-# Create your views here.
+class HomePageView(TemplateView):
+  template_name = 'home.html'
